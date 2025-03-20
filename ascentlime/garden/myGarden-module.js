442c559c228fe61217365d00e397ab9e36bae361
Eichi2514 <picon1317@gmail.com>
2025-03-20 20:26:52 +0900

    AscentLime : 정원(미니게임) 페이지 구현 중

---
+++ ascentlime/garden/myGarden-module.js
@@ -0,0 +1,101 @@
+// Firebase SDK 불러오기
+import {initializeApp} from "https://www.gstatic.com/firebasejs/11.1.0/firebase-app.js";
+import {
+    getDatabase,
+    ref,
+    query,
+    orderByChild,
+    equalTo,
+    get
+} from "https://www.gstatic.com/firebasejs/11.1.0/firebase-database.js";
+
+// Firebase 설정
+const firebaseConfig = {
+    apiKey: ".env/apiKey",
+    authDomain: ".env/authDomain",
+    databaseURL: "https://test-948ba-default-rtdb.firebaseio.com",
+    projectId: ".env/projectId",
+    storageBucket: ".env/storageBucket",
+    messagingSenderId: ".env/messagingSenderId",
+    appId: ".env/appId",
+    measurementId: ".env/measurementId"
+};
+
+// Firebase 초기화
+const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
+const membersRef = ref(database, 'members');
+
+const key = localStorage.getItem('nickname');
+if (!key) {
+    alert('로그인이 필요한 서비스 입니다');
+    window.location.href = '../../ascentlime.html';
+}
+
+window.getUserMoney = function (key) {
+    const queryRef = query(membersRef, orderByChild("key"), equalTo(key));
+    return get(queryRef)
+        .then((snapshot) => {
+            if (!snapshot.exists()) {
+                console.log('해당 아이디를 찾을 수 없습니다.');
+                return null;
+            }
+
+            const memberData = snapshot.val();
+            const memberKey = Object.keys(memberData)[0];
+            return memberData[memberKey].money;
+        })
+        .catch((error) => {
+            console.error("로그인 아이디 확인 중 오류 발생:", error);
+            return null;
+        });
+};
+
+function formatNumber(number) {
+    return number.toLocaleString();
+}
+
+const urls = window.location.search;
+const memberId = urls ? parseInt(urls.substring(1)) : 0;
+
+window.loginKeyCheckById = async function (key) {
+    const queryRef = query(membersRef, orderByChild("key"), equalTo(key));
+    try {
+        const snapshot = await get(queryRef);
+        if (!snapshot.exists()) {
+            console.log('loginKeyCheckById) 해당 키가 존재하지 않습니다.');
+            return null;
+        }
+
+        const memberData = snapshot.val();
+
+        const memberKey = Object.keys(memberData)[0];
+        return memberData[memberKey].id;
+    } catch (error) {
+        console.error("loginKeyCheckById) 해당 키 확인 중 오류 발생:", error);
+        return null;
+    }
+};
+
+let userMoney = 0;
+let gardenId = 0;
+
+async function loadUserData() {
+    try {
+        userMoney = await getUserMoney(key) || 0;
+        gardenId = await loginKeyCheckById(key);
+        let userMoneyString = formatNumber(userMoney);
+        $('.money_count').text(userMoneyString);
+
+        if (gardenId !== memberId) {
+            alert('입장 권한이 없습니다.');
+            window.location.href = '../../ascentlime.html';
+        }
+
+    } catch (error) {
+        console.error('돈 불러오는 중 오류 발생:', error);
+        $('.money_count').text('불러오기 실패');
+    }
+}
+
+loadUserData();
