e8feda7043eb1617d92b8f56d3581ec503bed05d
Eichi2514 <picon1317@gmail.com>
2025-03-23 22:34:28 +0900

    AscentLime : 식물 이미지 추가

--- ascentlime/garden/myGarden-module.js
+++ ascentlime/garden/myGarden-module.js
@@ -1,3 +1,29 @@
+const growthStages = [
+    'https://github.com/user-attachments/assets/96f61b3a-6413-434c-b756-668eb1d53617',
+    'https://github.com/user-attachments/assets/68170f34-d4bf-40f7-abcd-97a6fb5cc4dc',
+    'https://github.com/user-attachments/assets/7cbac15a-1b76-4fa8-92b2-fbe33dba9fe2',
+    'https://github.com/user-attachments/assets/3e11e472-a3e5-4ff7-aa8d-6ac53f8e8aec',
+    'https://github.com/user-attachments/assets/215a354a-b3f9-45be-b1d4-81665445898a',
+    'https://github.com/user-attachments/assets/7aa4ac5a-5219-4831-8c99-050e65647261',
+    'https://github.com/user-attachments/assets/f007d51f-f7be-4707-b459-25c5bc5f651e',
+    'https://github.com/user-attachments/assets/8f2dfff2-2a94-4da8-890e-f33c23b8d374',
+    'https://github.com/user-attachments/assets/d7d40f1f-3a62-4af2-abbf-fb7eba85696c',
+    'https://github.com/user-attachments/assets/513b7bf0-0aab-4a15-be8e-30de1472514d'
+];
+
+const fullyGrown = [
+    'https://github.com/user-attachments/assets/9361cc45-f251-4f92-99c2-c4fdd69a2970',
+    'https://github.com/user-attachments/assets/fac93404-41d6-4d9e-bfaa-8cfe788d7149',
+    'https://github.com/user-attachments/assets/50aa82dc-099a-47e5-8089-8df3af968ad2',
+    'https://github.com/user-attachments/assets/4b01953e-81a2-4a8d-8606-a75e5eef8919',
+    'https://github.com/user-attachments/assets/09be19ad-3fd4-4179-b575-f34000965c54',
+    'https://github.com/user-attachments/assets/c70737bb-ad9a-4edf-9f9c-fe8b7b1bae33',
+    'https://github.com/user-attachments/assets/21e3d1bb-e03d-4780-b0b7-4ef9d5354109',
+    'https://github.com/user-attachments/assets/e78bd9d2-cc77-4e97-abce-7bf33d5fba81',
+    'https://github.com/user-attachments/assets/973f70d5-ebab-4f8b-94d7-55e826035e1a',
+    'https://github.com/user-attachments/assets/bc64edff-7900-471b-9f03-31076e666053'
+];
+
 const plantItems = {
     "1": {
         "name": "5분",
